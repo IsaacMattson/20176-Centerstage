93654357d10e75a9961855f4d3e1c7d1b5812ada
Isaac <mattsonisaac06@gmail.com>
2023-12-20 16:33:49 -0700

    more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/AbsoluteOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/AbsoluteOpMode.java
@@ -1,22 +1,19 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
-public class OpMode extends LinearOpMode {
+public class AbsoluteOpMode extends LinearOpMode {
     private final double RIGHT_OPEN = 0.16;
     private final double RIGHT_CLOSE = 0.70;
     private final double LEFT_OPEN = 0.17;
@@ -48,7 +45,7 @@ public class OpMode extends LinearOpMode {
     private ColorSensor Color = null;
     private ColorRangeSensor ColorRange = null;
 
-    private IMU gyro = null;
+    private IMU imu = null;
 
     @Override
     public void runOpMode() {
@@ -65,12 +62,13 @@ public class OpMode extends LinearOpMode {
         liftLeft = hardwareMap.get(DcMotor.class, "LiftLeft");
         liftRight = hardwareMap.get(DcMotor.class, "LiftRight");
         plane = hardwareMap.get(Servo.class, "plane");
-        gyro = hardwareMap.get(IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
+
         IMU.Parameters parmeters= new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP
         ));
-        gyro.initialize(parmeters);
+        imu.initialize(parmeters);
         leftClaw.setDirection(Servo.Direction.REVERSE);
 
         //IMU
@@ -113,7 +111,7 @@ public class OpMode extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        gyro.resetYaw();
+        imu.resetYaw();
 
         waitForStart();
 
@@ -132,19 +130,25 @@ public class OpMode extends LinearOpMode {
             boolean liftStart = this.gamepad1.start;
             boolean launchPlane = this.gamepad1.right_stick_button;
 
-            // Movement code
-            double y = -gamepad1.left_stick_y; 
-            double x = gamepad1.left_stick_x * 1.1;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double leftFrontMotorPower = (y + x + rx) / denominator;
-            double leftBackMotorPower = (y - x + rx) / denominator;
-            double rightFrontMotorPower = (y - x - rx) / denominator;
-            double rightBackMotorPower = (y + x - rx) / denominator;
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            // Movement code
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double leftFrontMotorPower = (rotY + rotX + rx) / denominator;
+            double leftBackMotorPower = (rotY - rotX + rx) / denominator;
+            double rightFrontMotorPower = (rotY - rotX - rx) / denominator;
+            double rightBackMotorPower = (rotY + rotX - rx) / denominator;
 
             if(this.gamepad1.left_stick_button){
-                gyro.resetYaw();
+                imu.resetYaw();
             }
             //arm code
             if (armUp) {
@@ -152,13 +156,13 @@ public class OpMode extends LinearOpMode {
                 rotatorPosition = CLAW_UP;
             } else if (armDown) {
                 targetArmValue = ARM_DOWN;
-                closeBoth = true;
             }
             //servo code
             if (clawUp) {
                 rotatorPosition = CLAW_UP;
             } else if (clawDown) {
                 rotatorPosition = CLAW_DOWN;
+
             }
             if (leftOpen || openBoth) {
                 leftPosition = LEFT_OPEN;
